621774db22cd168929fcc661215cf96a409b6282
Gary Magill <Garymagill22@gmail.com>
2023-07-08 15:29:09 -0600

    DaikichiProject Routes

--- src/main/java/com/gary/daikichi/HomeController.java
+++ src/main/java/com/gary/daikichi/HomeController.java
@@ -1,7 +1,8 @@
 package com.gary.daikichi;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -20,4 +21,24 @@ public class HomeController {
         public String tomorrow() {
         	return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
         }
+        
+        
+        @RequestMapping("/travel/{city}")
+        public String travelToCity(@PathVariable String city) {
+        	return "Congratulations! You will soon travel to " + city + "!";
+        }
+
+        
+        
+        @RequestMapping("/lotto/{number}")
+        public String checkLottoNumber(@PathVariable int number) {
+        	if (number % 2 == 0) {
+        		return "You will take a grand journey in the near future, but be a weary of temping offers.";
+        	} else {
+        		return "You have enjoyed the fruits of your labor but now is a great time to spend with family and friends.";
+        	}
+        }
+
 }
+
+
